e7f24fd41299f365725a4f118ac3df1076730e03
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-01-13 11:24:49 -0800

    ...

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Defines all common attributes/methods for other classes.
 """
+Defines all common attributes/methods for other classes.
+"""
+
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -11,31 +12,30 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialization of a BaseModel instance.
-        Args:
-            **kwargs: dict of key-values arguments
-        """
-        if kwargs:
-            form = '%Y-%m-%dT%H:%M:%S.%f'
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at':
-                    self.created_at = datetime.strptime(
+    Args:
+    **kwargs: dict of key-values arguments
+    """
+    if kwargs:
+        form = '%Y-%m-%dT%H:%M:%S.%f'
+        for key, value in kwargs.items():
+            if key == '__class__':
+                continue
+            elif key == 'created_at':
+                self.created_at = datetime.strptime(
                         kwargs['created_at'], form)
-                elif key == 'updated_at':
-                    self.updated_at = datetime.strptime(
+            elif key == 'updated_at':
+                self.updated_at = datetime.strptime(
                         kwargs['updated_at'], form)
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+            else:
+                setattr(self, key, value)
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                models.storage.new(self)
 
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
-
         name = self.__class__.__name__
         return "[{}] ({}) {}".format(name, self.id, self.__dict__)
 
