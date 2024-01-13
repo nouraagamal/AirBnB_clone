1be54f10b2df922fcb7154051130d3494e7bd505
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-01-13 11:56:50 -0800

    ...

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,7 @@
 """
 Defines all common attributes/methods for other classes.
 """
-
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
@@ -13,27 +12,27 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialization of a BaseModel instance.
-    Args:
-    **kwargs: dict of key-values arguments
-    """
-    if kwargs:
-        form = '%Y-%m-%dT%H:%M:%S.%f'
-        for key, value in kwargs.items():
-            if key == '__class__':
-                continue
-            elif key == 'created_at':
-                self.created_at = datetime.strptime(
-                        kwargs['created_at'], form)
-            elif key == 'updated_at':
-                self.updated_at = datetime.strptime(
-                        kwargs['updated_at'], form)
-            else:
-                setattr(self, key, value)
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
-                models.storage.new(self)
+        Args:
+        **kwargs: dict of key-values arguments
+        """
+        if kwargs:
+            form = '%Y-%m-%dT%H:%M:%S.%f'
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(
+                            kwargs['created_at'], form)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                            kwargs['updated_at'], form)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
